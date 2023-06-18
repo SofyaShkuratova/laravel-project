6463fc995ee4dd2eb474e6ba145bfce72c9e56f2
Sofya Shkuratava <sofya.shkuratava@gmail.com>
2023-06-18 12:11:54 +0300

    added search for items

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\CategoryComponent;
+use App\Http\Livewire\SearchComponent;
 
 //Route::get('/', function () {
 //    return view('welcome');
@@ -20,6 +21,7 @@ Route::get('/product/{slug}', DetailsComponent::class)->name("product.details");
 Route::get('/cart', CartComponent::class)->name('shop.cart');
 Route::get('/checkout', CheckoutComponent::class)->name('shop.checkout');
 Route::get('/product-category/{slug}', CategoryComponent::class)->name('product.category');
+Route::get('/search', SearchComponent::class)->name('product.search');
 
 //Route::get('/dashboard', function () {
 //    return view('dashboard');
